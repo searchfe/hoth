57896849bbefa680faa295f81578172529448f7e
cxtom <chenxiao07@baidu.com>
2021-03-10 16:33:38 +0800

    chore: add pino final listen

--- packages/app-autoload/src/hook/preHandlerFactory.ts
+++ packages/app-autoload/src/hook/preHandlerFactory.ts
@@ -1,7 +1,9 @@
 import {FastifyReply, FastifyRequest} from 'fastify';
+import uuid from 'uuid-random';
 
 export default function (app: string) {
     return function (req: FastifyRequest, reply: FastifyReply, done) {
+        req.logid = req.logid || uuid();
         req.log = reply.log = reply.log.child({req, app});
         done();
     };
--- packages/cli/src/start.ts
+++ packages/cli/src/start.ts
@@ -3,7 +3,10 @@
  * @author cxtom
  */
 
+/* eslint-disable @typescript-eslint/await-thenable */
+
 import Fastify from 'fastify';
+import pino from 'pino';
 import isDocker from 'is-docker';
 import parseArgs from './parseArgs';
 import {exit, requireFastifyForModule} from '@hoth/utils';
@@ -50,11 +53,30 @@ async function runFastify(opts) {
         rootPath,
     });
 
+    const logger = createLogger({
+        apps,
+        rootPath,
+    });
+
+    // use pino.final to create a special logger that
+    // guarantees final tick writes
+    const handler = pino.final(logger, (err, finalLogger, evt) => {
+        finalLogger.info(`${evt} caught`);
+        if (err) {
+            finalLogger.error({err}, 'error caused exit');
+        }
+        process.exit(err ? 1 : 0);
+    });
+    // catch all the ways node might exit
+    process.on('beforeExit', () => handler(null, 'beforeExit'));
+    process.on('exit', () => handler(null, 'exit'));
+    process.on('uncaughtException', err => handler(err, 'uncaughtException'));
+    process.on('SIGINT', () => handler(null, 'SIGINT'));
+    process.on('SIGQUIT', () => handler(null, 'SIGQUIT'));
+    process.on('SIGTERM', () => handler(null, 'SIGTERM'));
+
     const fastifyInstance = fastify({
-        logger: createLogger({
-            apps,
-            rootPath,
-        }),
+        logger,
         disableRequestLogging: true,
         pluginTimeout: 60 * 1000,
     });

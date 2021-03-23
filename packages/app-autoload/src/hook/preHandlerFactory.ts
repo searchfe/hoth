a875c28a20f29444d10bae10dcb747bf08e29e5d
cxtom <chenxiao07@baidu.com>
2021-03-23 19:07:07 +0800

    feat(app-autoload): support fetch ral logid

--- packages/app-autoload/src/hook/preHandlerFactory.ts
+++ packages/app-autoload/src/hook/preHandlerFactory.ts
@@ -3,7 +3,10 @@ import uuid from 'uuid-random';
 
 export default function (app: string) {
     return function (req: FastifyRequest, reply: FastifyReply, done) {
-        req.logid = req.logid || (req.headers.logid as string) || uuid();
+        req.logid = req.logid
+            || (req.headers.x_bd_logid as string)
+            || (req.headers.logid as string)
+            || uuid();
         req.log = reply.log = reply.log.child({req, app});
         done();
     };

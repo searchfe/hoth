abe145e4074a4982186524d24770c0c90452d3f2
cxtom <cxtom2008@gmail.com>
2021-08-04 11:32:30 +0800

    perf(cli): update plugin example

--- packages/cli/templates/normal/src/plugin/foo/index.ts
+++ packages/cli/templates/normal/src/plugin/foo/index.ts
@@ -4,11 +4,12 @@
  */
 
 import type {FastifyInstance} from 'fastify';
+import fp from 'fastify-plugin';
 
-export default function (fastify: FastifyInstance, opts: any, next: any) {
+export default fp(function (fastify: FastifyInstance, opts: any, next: any) {
     console.log('foo plugin options', opts);
     next();
-}
+});
 
 export const autoConfig = {
     foo: 'bar',

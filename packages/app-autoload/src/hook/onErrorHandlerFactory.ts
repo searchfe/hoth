e7e9d5d3414aa4ed3423d40a5f50f7598af09b05
kaivean <kaivean@outlook.com>
2026-02-05 14:39:15 +0800

    add new version

--- packages/app-autoload/src/hook/onErrorHandlerFactory.ts
+++ packages/app-autoload/src/hook/onErrorHandlerFactory.ts
@@ -4,7 +4,7 @@ export default function (name: string) {
 
     // https://fastify.dev/docs/latest/Reference/Server/#seterrorhandler
     // setErrorHandler will not catch:
-    //  errors thrown in an onResponse hook because the response has already been sent to the client. Use the onSend hook instead.
+    // errors thrown in an onResponse hook because the response has already been sent to the client. Use the onSend hook instead.
     //  not found (404) errors. Use setNotFoundHandler instead.
     return function (error: Error, req: FastifyRequest, reply: FastifyReply) {
         /* istanbul ignore next */
